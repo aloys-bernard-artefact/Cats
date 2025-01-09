35a3727bbdf6f6676157abebaf836cd7dcc20fa8
Aloys Bernard <aloys.bernard@artefact.com>
2025-01-09 12:28:26 +0100

    [Add] Test & Linting

---
+++ src/operations.py
@@ -0,0 +1,16 @@
+
+
+def add(num1,num2):
+    return num1+num2
+
+def subtract(num1,num2):
+    #TODO: Implement this method
+    pass
+
+def multiply(num1,num2):
+    #TODO: Implement this method
+    pass
+
+def divide(num1,num2):
+    # TODO: Implement this method
+    pass
